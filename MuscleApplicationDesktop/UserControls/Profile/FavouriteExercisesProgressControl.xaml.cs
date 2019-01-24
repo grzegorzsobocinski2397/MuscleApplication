2930484839807fbdcefae70db18267c62d0e692b
grzegorzsobocinski2397 <43864376+grzegorzsobocinski2397@users.noreply.github.com>
2019-01-24 18:47:06 +0100

    Added user information and favourite exercises progress controls.

---
+++ MuscleApplicationDesktop/UserControls/Profile/FavouriteExercisesProgressControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows.Controls;
+
+namespace MuscleApplication.Desktop
+{
+    /// <summary>
+    /// Interaction logic for UserInformationControl.xaml
+    /// </summary>
+    public partial class FavouriteExercisesProgressControl : UserControl
+    {
+        public FavouriteExercisesProgressControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MuscleApplicationDesktop/UserControls/Profile/UserInformationControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows.Controls;
+
+namespace MuscleApplication.Desktop
+{
+    /// <summary>
+    /// Interaction logic for UserInformationControl.xaml
+    /// </summary>
+    public partial class UserInformationControl : UserControl
+    {
+        public UserInformationControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
